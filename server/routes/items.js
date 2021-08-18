14abef380164efc75ce969e2b0da3e12f6448c0c
Houssame Zerouali <azenax328@gmail.com>
2021-08-18 10:47:12 +0200

    merge conflict

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const Item = require('../models/Item')
+const Item = require('../models/Item');
+const requireAuth = require('../middlewares/requireAuth')
+const mongoose = require('mongoose')
 
-router.get('/api/items', (req, res, next) => {
+router.get('/api/items', requireAuth, (req, res, next) => {
     Item
     .find()
     .then((itemsDocument) => {
@@ -10,10 +12,10 @@ router.get('/api/items', (req, res, next) => {
     })
     .catch((err) => {
         res.status(500).json(err)
-    })
+    })  
 })
 
-router.get('/api/items/:id', (req, res, next) => {
+router.get('/api/items/:id', requireAuth,  (req, res, next) => {
     Item.findOneById(req.params.id)
     .then((foundItem) => {
         if(!foundItem) {
@@ -26,7 +28,7 @@ router.get('/api/items/:id', (req, res, next) => {
     })
 })
 
-router.post('/api/items', (req, res, next) => {
+router.post('/api/items', requireAuth, (req, res, next) => {
     Item.create(req.body)
     .then((createdItem) => {
         res.status(201).json(createdItem);
@@ -36,7 +38,7 @@ router.post('/api/items', (req, res, next) => {
     })
 })
 
-router.patch('/api/items/:id', async (req, res, next) => {
+router.patch('/api/items/:id', requireAuth, async (req, res, next) => {
 
     try {
         const updated = await Item.findByIdAndUpdate(id, req.body, {
@@ -49,7 +51,7 @@ router.patch('/api/items/:id', async (req, res, next) => {
 });
 
 
-router.delete('/api/items/:id', (req, res, next) => {
+router.delete('/api/items/:id', requireAuth, (req, res, next) => {
     Item.findByIdAndDelete(req.params.id).then((deletedItem) => {
         if(!deletedItem) {
             res.status(404).json({message : 'This item does not exist'})
